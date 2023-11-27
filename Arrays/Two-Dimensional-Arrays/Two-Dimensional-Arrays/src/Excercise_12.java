7d0be965dd555639283d61dc1050fbab44c9c3ea
aliaksei.dubanevich <aliaksei.dubanevich@student.ehu.lt>
2023-11-27 18:06:31 +0300

    add Exc12

--- Arrays/Two-Dimensional-Arrays/Two-Dimensional-Arrays/src/Excercise_12.java
+++ Arrays/Two-Dimensional-Arrays/Two-Dimensional-Arrays/src/Excercise_12.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Excercise_12 {
     public static void main(String[] args){
-        int n = 5;
+        int n = 6;
         int[][] a = Solution_12.create(n);
         System.out.println(Arrays.deepToString(a));
     }
@@ -17,12 +17,35 @@ class Solution_12 {
     public static int[][] create(int n) {
         //put your code here
         int[][] a = new int [n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i+j == n) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n / 2; j++){
+                if (i <= j){
                     a[i][j] = 1;
                 } else {
                     a[i][j] = 0;
+                }
+            }
+            for (int j = n / 2; j < n; j++){
+                if (i >= j - n/2){
+                    a[i+n/2][j] = 1;
+                } else {
+                    a[i+n/2][j] =0;
+                }
+            }
+        }
+        for (int i = n / 2; i < n; i++) {
+            for (int j = 0; j < n / 2; j++){
+                if (i-n/2 <= j){
+                    a[i][j] = 1;
+                } else {
+                    a[i][j] = 0;
+                }
+            }
+            for (int j = n / 2; j < n; j++){
+                if (i >= j){
+                    a[i][j] = 1;
+                } else {
+                    a[i/2][j] =0;
                 }
             }
         }
